fa15ca918cfa9bdbd577a0fb09e9ade0d7609a63
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2024-12-25 13:47:59 -0600

    Update profileRoutes.js

--- widgets/charGen/profileRoutes.js
+++ widgets/charGen/profileRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", pc.profileHome);
 router.post("/create", pc.createProfile)
-router.get("/:name", pc.getProfile);
+router.get("/:name/:id", pc.getProfile);
 router.get("/recent/:amount", pc.getRecentProfiles); 
 
 export default router;

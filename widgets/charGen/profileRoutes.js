7f60b3918e4c66a7d11c2c47bd90459a2242b8ee
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2024-12-25 15:29:01 -0600

    Update profileRoutes.js

--- widgets/charGen/profileRoutes.js
+++ widgets/charGen/profileRoutes.js
@@ -4,9 +4,9 @@ import * as pc from "./profileController.js";
 const router = express.Router();
 
 router.get("/", pc.profileHome);
-router.post("/create", pc.createProfile)
-router.get("/:name/:id", pc.getProfile);
-router.get("/recent/:amount", pc.getRecentProfiles); 
-router.delete("/delete/:name", pc.deleteProfile); //need to have a check if current token id matches profile user id?
+router.post("/create/:user_id", pc.createProfile)
+router.get("/:user_id/:name/:id", pc.getProfile);
+router.get("/recent/:user_id/:amount", pc.getRecentProfiles); 
+router.delete("/delete/:user_id/:name", pc.deleteProfile); //need to have a check if current token id matches profile user id?
 
 export default router;

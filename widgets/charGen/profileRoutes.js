309da4e563f31c5dcf900c46dd982d30a5545651
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2024-12-25 13:56:30 -0600

    Update profileRoutes.js

--- widgets/charGen/profileRoutes.js
+++ widgets/charGen/profileRoutes.js
@@ -7,6 +7,6 @@ router.get("/", pc.profileHome);
 router.post("/create", pc.createProfile)
 router.get("/:name/:id", pc.getProfile);
 router.get("/recent/:amount", pc.getRecentProfiles); 
-router.delete("/delete/:name/:id", pc.deleteProfile); //need to have a check if current token id matches profile user id?
+router.delete("/delete/:name", pc.deleteProfile); //need to have a check if current token id matches profile user id?
 
 export default router;

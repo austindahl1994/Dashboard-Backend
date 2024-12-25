0a137f6151c571ecb25cd972e9eadaf44c5d0d9c
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2024-12-25 15:25:17 -0600

    Update profile.js

--- widgets/charGen/profile.js
+++ widgets/charGen/profile.js
@@ -1,10 +1,10 @@
 import pool from '../../db/mysqlPool.js'
 
-const createProfile = async (name, data) => {
+const createProfile = async (user_id=1, name, data) => {
     console.log(`Attempting to create profile with name: ${name}`)
-    const query = 'INSERT INTO profiles (name, properties) VALUES(?, ?)'
+    const query = 'INSERT INTO profiles (name, properties) VALUES(?, ?) WHERE user_id=?'
     try {
-        const result = pool.execute(query, [name, JSON.stringify(data)])
+        const result = pool.execute(query, [user_id, name, JSON.stringify(data)])
         return result.affectedRows;
     } catch (error) {
         throw e
@@ -12,11 +12,12 @@ const createProfile = async (name, data) => {
     
 }
 
-const getProfile = async (name, id=-1) => {
+//change user_id after initial tests are done
+const getProfile = async (user_id=1, name, id=-1) => {
     console.log(`Attempting to query profiles for name: ${name}`)
-    const query = 'SELECT * FROM profiles WHERE name=? OR id=?'
+    const query = 'SELECT * FROM profiles WHERE name=? OR id=? AND user_id=?'
     try {
-        const [rows] = await pool.execute(query, [name, id])
+        const [rows] = await pool.execute(query, [name, id, user_id])
         return rows[0]
     } catch (error) {
         console.log(`There was an error: ${error}`)
@@ -24,21 +25,24 @@ const getProfile = async (name, id=-1) => {
     }
 }
 
-const getRecentProfiles = async (amount) => {
+const getRecentProfiles = async (user_id=1, amount) => {
+    const query = 'SELECT * FROM profiles WHERE user_id=? ORDER BY time_updated DESC LIMIT ?'
     try {
-        const data = await fromwhere
+        const rows = await pool.execute(query, [user_id, amount])
+        return rows
     } catch (error) {
-        console.error(`There was an error: ${error}`)
+        console.error(`There was an error getting the rows: ${error}`)
         throw e
     }
 }
 
-const updateProfile = async (name, properties) => {
-    const query = 'UPDATE profiles SET properties=? WHERE name=?'
+const updateProfile = async (user_id=1, name, properties) => {
+    const query = 'UPDATE profiles SET properties=? WHERE name=? AND user_id=?'
     try {
         const result = await pool.execute(query, [
           JSON.stringify(properties),
           name,
+        	user_id
         ]);
         return result.affectedRows //affectedRows
     } catch (error) {
@@ -47,10 +51,10 @@ const updateProfile = async (name, properties) => {
     }
 }
 
-const deleteProfile = async (name) => {
-    const query = 'DELETE FROM profiles WHERE name=?'
+const deleteProfile = async (user_id=1, name) => {
+    const query = 'DELETE FROM profiles WHERE name=? AND user_id=?'
     try {
-        const result = await pool.execute(profiles, [name])
+        const result = await pool.execute(profiles, [name, user_id])
         return result.affectedRows
     } catch (error) {
         console.error(`Database error: ${error}`)

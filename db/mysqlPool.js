6a589eec216eb00bb2d907ff1b3b3b54f064f9e7
Austin Dahl <austindahl94@gmail.com>
2025-02-19 15:28:14 -0600

    Update mysqlPool.js

--- db/mysqlPool.js
+++ db/mysqlPool.js
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0

ca382b59eae0a4feb6079510ea70f6ccb3b385dd
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-04-12 11:54:35 -0500

    Update expenseRoutes.js
    added year and month passed in via params instead of body

--- widgets/expenseTracker/expenseRoutes.js
+++ widgets/expenseTracker/expenseRoutes.js
@@ -4,7 +4,7 @@ import * as ec from './expenseController.js
 const router = express.Router()
 
 router.get("/get", ec.getExpenses)
-router.put("/update", ec.updateExpenses)
+router.put("/update/:year/:month", ec.updateExpenses)
 router.delete("/delete/:year/:month", ec.deleteExpenses)
 
 export default router

1dabeda3fbad3be4da9cfe8f7412d77df4540635
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-04-12 12:16:41 -0500

    Update expenses.js
    updating Model for expenses

--- widgets/expenseTracker/expenses.js
+++ widgets/expenseTracker/expenses.js
@@ -1,12 +1,34 @@
-//get all expenses from db, small enough data, let frontend filter/use what it wants
-// export const getExpenses = (user_id) => {
-// 	const query = 'SELECT * FROM expenses WHERE user_id = ?' 
-// 	try {
-// 	const [rows] = pool.execute(query[user_id])
-// 	return rows
-// } catch (error) {
-// 	throw error
-// }
-// }
+import pool from "../../db/mysqlPool.js";
 
-//INSERT ON DUPLICATE KEY for updating data
+export const getExpenses = async (user_id) => {
+	const query = 'SELECT * FROM expenses WHERE user_id = ?' 
+	try {
+	  const [rows] = pool.execute(query[user_id])
+	  return rows
+  } catch (error) {
+	  throw error
+  }
+}
+
+export const updateExpense = async (user_id, month, year, data) => {
+  const query = 'REPLACE INTO expenses (user_id, month, year, data) VALUES (?, ?, ?, ?)'
+  try {
+    const response = await pool.execute(query, [user_id, month, year, data])
+    return response
+  } catch (error) {
+    throw new Error("Could not update expense data in database")
+  }
+}
+
+export const deleteExpense = async (user_id, month, year) => {
+  const query = "DELETE FROM expenses WHERE month=? AND year=? AND user_id=?";
+  try {
+    const [result] = await pool.execute(query, [month, year, user_id]);
+    return result;
+  } catch (error) {
+    console.error(`Database error: ${error}`);
+    throw error;
+  }
+}
+
+//INSERT ON DUPLICATE KEY for updating data, but since not UNIQUE or PK, using REPLACE INTO

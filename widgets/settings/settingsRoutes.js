2119caa1bd08517e2d61cd1e54f2b946a4f8278f
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-04-12 15:24:51 -0500

    Update settingsRoutes.js

--- widgets/settings/settingsRoutes.js
+++ widgets/settings/settingsRoutes.js
@@ -1,1 +1,9 @@
+import * as sc from './settingsController.js'
+import Express from 'express'
 
+const router = express.Router()
+
+router.put('/updateSetting/:widgetName', sc.updateSettings)
+router.delete('deleteSettings/:widgetName', sc.deleteSettings)
+
+export default router

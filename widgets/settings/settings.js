b34c52261f242692dbb6a5bb9f166b8f93e0fe0a
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-04-12 15:03:18 -0500

    Update settings.js

--- widgets/settings/settings.js
+++ widgets/settings/settings.js
@@ -1,1 +1,21 @@
+import pool from '../../db/mysqlPool.js'
 
+export const updateSettings = async (user_id, widgetName, data) => {
+  const query = "REPLACE INTO widgets (user_id, widgetName, data) VALUES (?, ?, ?)"
+  try {
+    const response = await pool.execute(query, [user_id, widgetName, data])
+    return response
+  } catch (error) {
+    throw new Error(`Could not update widget settings`)
+  }
+}
+
+export const deleteSettings = async (user_id) => {
+  const query = "DELETE FROM widgets WHERE user_id=?"
+  try {
+    const response = await pool.execute(query, [user_id])
+    return response
+  } catch (error) {
+    throw new Error(`Could not delete widget settings`)
+  }
+}

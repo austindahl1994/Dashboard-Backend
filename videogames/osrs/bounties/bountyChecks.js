c36810b66ab3ca03c939ce54ca9b58c4bae3e8a1
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-07-26 15:01:05 -0500

    Update bountyChecks.js

--- videogames/osrs/bounties/bountyChecks.js
+++ videogames/osrs/bounties/bountyChecks.js
@@ -2,31 +2,31 @@ import { updateSimpleSources } from "./bountyUpdates.js";
 import * as compare from "./compareData.js"
 
 //TODO: Figure out when we should check simple cached data, whenever bounties are updated I think would be best
+// change to update on bounty update instead
+// if (simpleTypes.length === 0) {
+//     updateSimpleTypes();
+//   }
+const handlers = {
+  LOOT: compare.loot,
+  CLUE: compare.clue,
+  DEATH: compare.death,
+  PET: compare.pet,
+  SPEEDRUN: compare.speedrun,
+  BARBARIAN_ASSAULT_GAMBLE: compare.ba,
+  PLAYER_KILL: compare.pk
+};
 
 const checkBounties = (data) => {
-  if (simpleTypes.length === 0) { //change to update on bounty update instead
-    updateSimpleTypes();
-  }
   if (!data.type || data.type.trim() === "") {
-    throw new Error("No type was passed into ")
+    throw new Error("No type was passed in")
   } else {
-    const handlers = {
-      LOOT: compare.loot,
-      CLUE: compare.clue,
-      DEATH: compare.death,
-      PET: compare.pet,
-      SPEEDRUN: compare.speedrun,
-      BARBARIAN_ASSAULT_GAMBLE: compare.ba,
-      PLAYER_KILL: compare.pk
-  };
-
-  const trimmedType = type.trim(); 
-  const handler = handlers[trimmedType];
-
-  if (handler) {
-    return handler(data); 
-  } else {
-    throw new Error(`Type ${type} is recognized but has no specific check implemented.`);
-  }
+    const trimmedType = type.trim(); 
+    const handler = handlers[trimmedType];
+  
+    if (handler) {
+      return handler(data); 
+    } else {
+      throw new Error(`Type ${type} is recognized but has no specific check implemented.`);
+    }
   }
 };

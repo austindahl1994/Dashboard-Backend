34fca0fcd5695045bf4d9b7797bf2ee99b555d47
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-08-03 16:42:13 -0500

    Create skipBounty.js

---
+++ bot/commands/skipBounty.js
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder, MessageFlags } from "discord.js";
+
+export default {
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("Skip a bounty manually"),
+  async execute(interaction) {
+    if (interaction.user.id !== allowedUserId) {
+      return interaction.reply({
+        content: "Yo fuckoff, you don't need to be here right now.",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+    // locic to manually skip a bounty, based on array of allowed discord_ids since can't use mod ID?
+    // or if we can use based on their permissions: member.permissions.has('SomePermission')
+    await interaction.reply("Need skip logic");
+  },
+};

7553bee94e031c68c19dcaf175fe1fb7f2c39c1c
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-08-31 15:38:53 -0500

    Create updateTeamName.js
    Added bot command to change team name

---
+++ bot/commands/updateTeamName.js
@@ -0,0 +1,69 @@
+import { SlashCommandBuilder, MessageFlags } from "discord.js";
+import { allowedUserIds } from "../utilities/discordUtils.js";
+import { teamNames } from "../../videogames/osrs/cachedData.js"
+import { updateTeamName } from "../../videogames/osrs/data/discordMembers.js";
+
+export default {
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName("groupname")
+    .setDescription("Change the Bounty Event group name for a team")
+    .addStringOption((option) => 
+      option
+        .setName("oldName")
+        .setDescription("Select a current team name to change: ")
+        .setRequired(true)
+        .setAutocomplete(true)
+    )
+    .addStringOption((option) => 
+      option
+        .setName("newName")
+        .setDescription("Updated to name: ")
+        .setRequired(true)
+    ),
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+
+    const choices = teamNames.map((name) => {
+      return {
+        name: name,
+        value: name,
+      };
+    });
+  
+    const filtered = choices.filter((choice) =>
+      choice.name.toLowerCase().includes(focusedValue.toLowerCase())
+    );
+
+    await interaction.respond(
+      filtered.slice(0, 25)
+    );
+  },
+  
+  async execute(interaction) {
+    try {
+      if (!allowedUserIds.includes(interaction.user.id)) {
+        return interaction.reply({
+          content: "⛔ You are not allowed to use this command.",
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+      await interaction.deferReply({
+        content: "Attempting to update team name...",
+        flags: MessageFlags.Ephemeral
+      })
+      const prevName = interaction.options.getString("oldName")
+      const newName = interaction.options.getString("newName")
+      await updateTeamName(prevName, newName)
+      await interaction.editReply({
+        content: "Updated team name!"
+        flags: MessageFlags.Ephemeral,
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: `There was an error creating groups: ${error}`,
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  },
+};

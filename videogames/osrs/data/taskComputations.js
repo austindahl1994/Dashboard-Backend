bd83f9af431141ae4546a78a63a0fc848a41a92f
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-08-31 16:49:23 -0500

    Update taskComputations.js
    Added merging of finalTasks into each individual task sheet

--- videogames/osrs/data/taskComputations.js
+++ videogames/osrs/data/taskComputations.js
@@ -1,12 +1,16 @@
-import { getFinalTasks } from "../../../services/google/osrsSheets.js";
+import { getFinalTasks, migrateTasks } from "../../../services/google/osrsSheets.js";
 import { getTier } from "../bounties/bountyUtilities.js";
 
+const FINAL_TASKS_LENGTH = 180
+const MAX_TASKS_IN_TIER = 100
 //Take in final tasklist, need following:
 // How many times the items appear
 // How many tasks are in each tier
 export const finalTasks = async () => {
   try {
-    const allTasks = await getFinalTasks(); //returned as array of arrays, headers are first row
+    const allItems = new Set([])
+    const range = ["final!B2:B400", "final!F2:F400"];
+    const allTasks = await getFinalTasks(range); //returned as array of arrays, headers are first row
     // console.log(allTasks); //[[[item1], [item2], [item3]],[[difficulty1],[difficulty2]]]
     const difficultyCount = new Map();
     const itemCounts = {};
@@ -21,6 +25,7 @@ export const finalTasks = async () => {
             const items = itemString.split(",");
             items.forEach((i) => {
               let trimmedItem = i.trim().toLowerCase().replace(/[’‘]/g, "'");
+              allItems.add(trimmedItem)
               if (itemCounts[trimmedItem]) {
                 itemCounts[trimmedItem] += 1;
               } else {
@@ -78,8 +83,39 @@ export const finalTasks = async () => {
     console.table(itemsArr);
     console.log("Number of tasks in tier: ");
     console.table(difficultyArr);
+    console.log("All items: ")
+    console.log([...allItems].sort().join(", "))
   } catch (error) {
     console.log(`Error calling finalTasks: `);
     console.log(error);
   }
 };
+
+//TODO: Add actual logic to migrateTasks
+//TODO: Add PK tasks based on what adam said
+//TODO: Add seaking death tasks
+//TODO: Remove anything from source unless it has a specific source!
+//TODO: Update bountyCompletion embeds to represent correct task, maybe include difficulty, team name, etcAdded merging of finalTasks into each individual task sheet
+const mergeFinalTasks = async () => {
+  try {
+    const tasks = Array.from({length: 5}).map(() => [])
+    const finalTasks = await getFinalTasks([`final!A2:H${FINAL_TASKS_LENGTH}`]);
+    finalTasks.forEach(([title, items, source, description, type, difficulty, other, url]) => {
+      //index: Title, Items, Source, Description, Type, Difficulty, Other, Wiki_URL
+      tasks[difficulty - 1].push([
+        title, items, source, description, type, 0, url, other
+      ])
+    })
+    const ranges = Array.from({ length: 5 }).map((_, i) => `${getTier(i)}:A2:H${MAX_TASKS_IN_TIER}`)
+    const batchedData = ranges.map((range, index) => {
+      return {
+        range: range,
+        values: tasks[index]
+      }
+    })
+    await migrateTasks(batchedData)
+  } catch (error) {
+    console.log(`Error merging tasks: ${error}`)
+    throw error
+  }
+}

52c274d6422eed2ede381761f3f19b1d4a71e6b3
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-09-04 20:36:52 -0500

    Create cachingData.js

---
+++ videogames/osrs/data/cachingData.js
@@ -0,0 +1,26 @@
+import { getAllSheetData } from "../../../services/google/osrsSheets.js"
+import { modifySheetData } from "../bounties/updateFromSheets.js"
+import { updateCachedMembers } from "./discordMembers.js"
+
+export const getCachedData = async () => {
+  try {
+    // cachedBounties, highscores, numberOfBounties should all be updated from updateSheetsFunction
+    const sheets = ["easy", "medium", "hard", "elite", "master", "members"];
+    const ranges = ["A2:L75","A2:L75","A2:L75","A2:L75","A2:L75","A2:H500"]
+    const finalData = sheets.map((sheet, i) => {
+      return `${sheet[i]}!${ranges[i]}`
+    })
+    const allData = await getAllSheetData(finalData)
+    const memberData = allData.pop()
+    modifySheetData(allData)
+    updateCachedMembers(memberData)
+    //Need to cache player information format will
+  } catch (e) {
+    console.log("Error getting cached data: ")
+    console.log(e)
+  }
+}
+
+export const postPlayerRequests = async () => {
+  
+}

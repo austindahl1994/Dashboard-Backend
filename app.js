6579ccf4082ce577053e302b949887ee1f2bc730
Austin Dahl <austindahl94@gmail.com>
2025-09-08 15:54:04 -0500

    updated size limit

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(limiter);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(
   cors({
     origin: process.env.ORIGIN,

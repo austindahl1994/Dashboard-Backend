a9e5e153c791c2e4fa5fb0e5ce6ac329e7d8ba01
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-09-14 12:36:03 -0500

    Update gpEmbed.js
    Removed misspelling of buyinGP and improved code appearance

--- bot/embeds/gpEmbed.js
+++ bot/embeds/gpEmbed.js
@@ -4,19 +4,21 @@ export const gpEmbed = (players) => {
   const paidMembers = players.map((p) => {
     return `RSN: ${p.rsn} Donation: ${p.donation} Time: ${p.time} Discord: ${p.nickname}`
   }).join("\n")
+  
   const buyinGP = players.length * 11
   const donationGP = players.reduce((p, acc) => (parseInt(p.donation) || 0) + acc, 0)
   const totalGP = buyinGP + donationGP
+  
   const embed = new EmbedBuilder()
     .setTitle(`__Paid members__`)
     .addFields({
       name: "__Players__",
-      value: `${paidMembers}`,
+      value: paidMembers,
       inline: true,
     })
     .addFields({
       name: "__Total GP__",
-      value: `Buyins: ${buyingGP} Donations: ${donationGP} Total: ${totalGP}`,
+      value: `Buyins: ${buyinGP} Donations: ${donationGP} Total: ${totalGP}`,
       inline: false
     })
   return [embed];

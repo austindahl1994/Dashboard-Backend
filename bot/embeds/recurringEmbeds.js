0747e58ac749450f3e8a519052f6de28f158674a
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-09-27 13:15:50 -0500

    Create recurringEmbeds.js
    Start of the recurring embeds

---
+++ bot/embeds/recurringEmbeds.js
@@ -0,0 +1,26 @@
+import { EmbedBuilder } from "discord.js";
+import { recurring } from "../../videogames/osrs/cachedBounty.js"
+
+export const recurringEmbed = () => {
+  try {
+    
+  } catch (e) {
+    console.error(`Error creating recurring bounties embed: ${error}`);
+  }
+}
+
+export const emptyRecurringEmbed = () => {
+  try {
+    
+  } catch (e) {
+    console.error(`Error creating empty recurring embed: ${error}`);
+  }
+}
+
+export const completedRecurring = () => {
+  try {
+    
+  } catch (e) {
+    console.error(`Error creating completed recurring embed: ${error}`);
+  }
+}

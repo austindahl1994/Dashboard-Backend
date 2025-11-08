4242bf9fcf0d311c5cf325854899efd0d840d3ba
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-11-08 11:20:11 -0600

    Add boardFunctions.ts with initial function stubs

---
+++ vingo/boardFunctions.ts
@@ -0,0 +1,31 @@
+import { Board, Players } from "./cachedData.ts"
+
+export const getBoardData = async () => {
+  try {
+    
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
+
+// After board data is pulled, update cached board data
+const updateCachedBoard = () => {
+  try {
+    
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
+
+// Called when team data is gotten
+// Passed in all data for that team, will calc points based on cached board data
+export const calcPoints = () => {
+  try {
+    
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}

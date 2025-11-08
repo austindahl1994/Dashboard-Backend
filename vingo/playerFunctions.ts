09530d3775fb2f8c1ed6f10933eacf9fcfdee89d
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-11-08 11:20:21 -0600

    Refactor player data functions in playerFunctions.ts

---
+++ vingo/playerFunctions.ts
@@ -0,0 +1,41 @@
+import { Board, Players } from "./cachedData.ts"
+
+// Gets player data from sheets
+export const getPlayerData = async () => {
+  try {
+    
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
+
+// Updates player data from buyins
+export const updatePlayerData = async () => {
+  try {
+    
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
+
+// After player data is obtained from sheets, cache it
+export const updateCachedPlayers = () => {
+  try {
+    
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
+
+// Gets player data by discord_id
+export const getPlayer = () => {
+  try {
+    
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
